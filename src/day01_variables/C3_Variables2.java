4e5ca5211adfcb7a36d9956b9ab603571033eb8a
Ahmet Demir <demir45@yahoo.com>
2021-09-01 19:16:44 +0300

    version_01

---
+++ src/day01_variables/C3_Variables2.java
@@ -0,0 +1,23 @@
+package day01_variables;
+
+
+public class C3_Variables2 {
+
+	public static void main(String[] args) {
+		
+		String ismim="Ahmet";
+		String soyisim="Demir";
+		
+		System.out.print(ismim);
+		System.out.println(soyisim);
+		
+		
+		System.out.println(ismim + ' ' + soyisim);
+		// String degiskenlerde + islemi yaparsak, Java degiskenleri art arda ekler
+		
+		char bosluk = ' ';
+		
+		System.out.println(ismim+bosluk+soyisim);
+		
+	}
+}
